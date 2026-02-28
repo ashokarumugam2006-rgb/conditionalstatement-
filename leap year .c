3d73a7665cec4553338f29c2ef88468129336675
Ashok kumar <ashokarumugam2006@gmail.com>
2026-02-28 15:26:20 +0530

    new code

---
+++ leap year .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("Enter a century year: ");
+    scanf("%d", &year);
+
+    if (year % 100 != 0) {
+        printf("%d is not a century year.\n", year);
+    }
+    else {
+        if (year % 400 == 0) {
+            printf("%d is a Leap Year.\n", year);
+        }
+        else {
+            printf("%d is NOT a Leap Year.\n", year);
+        }
+    }
+
+    return 0;
+}
