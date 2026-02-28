825a23990d88e0ada0d3447b95fcff87c7b946ae
Ashok kumar <ashokarumugam2006@gmail.com>
2026-02-28 16:14:01 +0530

    new code

---
+++ bitwise .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    // Swapping using XOR
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    printf("After swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
